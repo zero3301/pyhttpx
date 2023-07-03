67456d8d75be6b539296482f045fe55166248f03
zero3301 <108772273+zero3301@users.noreply.github.com>
2023-07-03 18:34:45 +0800

    Update websocketTest.py

--- tests/websocketTest.py
+++ tests/websocketTest.py
@@ -15,14 +15,11 @@ class WSS:
         self.url = url
         self.headers = headers
         self.loop = loop
-        self.ja3 = '771,19018-4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-17513,27242-29-23-24,0'
-
     async def connect(self):
 
         self.sock = await WebSocketClient(url=self.url,
                                           headers=self.headers,
                                           loop=self.loop,
-                                          ja3=self.ja3
                                           ).connect()
 
         print('连接成功...')
